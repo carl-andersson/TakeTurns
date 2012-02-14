e3e1c30e7b45135efa148e4a7b21e280017de221
Sebastian Ärleryd <baskus@gmail.com>
2012-02-14 11:50:49 +0100

    Initial commit
    Added Game class stub and very basic hooks into gdt.

---
+++ src/start.cpp
@@ -0,0 +1,61 @@
+/*
+ * start.cpp
+ *
+ * Copyright (c) 2011 Sebastian Ärleryd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "gdt.h"
+
+#include "Game.h"
+
+Game *game;
+
+static void on_touch(touch_type_t what, int screenX, int screenY) {
+}
+
+void gdt_hook_initialize() {
+	game = new Game();
+	game->init();
+}
+
+void gdt_hook_visible(bool newSurface, int width, int height) {
+	game->visible(newSurface, width, height);
+}
+
+void gdt_hook_active() {
+
+}
+
+void gdt_hook_inactive() {
+
+}
+
+void gdt_hook_save_state() {
+
+}
+
+void gdt_hook_hidden() {
+
+}
+
+void gdt_hook_render() {
+
+}
