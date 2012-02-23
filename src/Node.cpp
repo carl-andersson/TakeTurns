83e086143d6886fa2392696ed9d9f3e8ade9de67
Sebastian Ärleryd <baskus@gmail.com>
2012-02-23 22:09:04 +0100

    Added simple error checking when loading textures. Renamed widget to Node. Other minor changes.

---
+++ src/Node.cpp
@@ -0,0 +1,44 @@
+/*
+ * Node.cpp
+ *
+ * CopyRight (c) 2012 Carl Andersson
+ * CopyRight (c) 2012 Sebastian Ärleryd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "Node.h"
+
+const string_t Widget::TAG = "Node";
+
+Shader Widget::sShader;
+
+void Widget::draw() {
+	sShader.setAttribute4f("translation",mX,mY,0,0);
+	sShader.setAttribute4f("vert_color",mColorRed,mColorGreen,mColorBlue,mColorAlpha);
+	sShader.setAttribute2f("scale",mScaleX,mScaleY);
+
+	selfDraw();
+
+	for (int i = 0; i < mChildren.size(); i++){
+		mChildren[i]->draw();
+	}
+
+	sShader.setAttribute4f("translation",-mX,-mY,0,0);
+}
---
+++ src/Node.h
@@ -0,0 +1,88 @@
+/*
+ * Widget.h
+ *
+ * CopyRight (c) 2012 Carl Andersson
+ * CopyRight (c) 2012 Sebastian Ärleryd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <vector>
+#include <string.h>
+#include <gdt/gdt.h>
+
+#include "Shader.h"
+
+class Node {
+
+private:
+	static const string_t TAG;
+
+	std::vector<Node*> mChildren;
+
+protected:
+	static Shader sShader;
+	//Shader mShader;
+	/*Node(Shader shader){
+		mShader=shader;
+		mShader=sShader;
+		children=std::vector<Widget*>();
+		mX=0;
+		mY=0;
+		mScaleX=1;
+		mScaleY=1;
+		mColorGreen=1;
+		mColorBlue=1;
+		mColorRed=1;
+		mColorAlpha=1;
+	}*/
+
+public:
+	float mX;
+	float mY;
+
+	float mColorGreen;
+	float mColorRed;
+	float mColorBlue;
+	float mColorAlpha;
+
+	float mScaleX;
+	float mScaleY;
+
+	Node() {
+		//mShader=sShader;
+		children = std::vector<Node*>();
+		mX = 0;
+		mY = 0;
+		mScaleX = 1;
+		mScaleY = 1;
+		mColorGreen = 1;
+		mColorBlue = 1;
+		mColorRed = 1;
+		mColorAlpha = 1;
+	}
+
+	virtual void draw();
+	virtual void selfDraw(){gdt_log(LOG_NORMAL, TAG, "selfDraw");};
+};
+
+#endif //WIDGET_H
